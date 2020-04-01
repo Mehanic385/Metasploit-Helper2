f8cf0c63722a67011e78b21b2822dc0975dbbb88
Mehanic385 <58910349+Mehanic385@users.noreply.github.com>
2020-04-01 19:30:54 +0700

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,11 @@
+#../usr/bin/bash
+
+pkg update && pkg upgrade
+pkg install lolcat
+pkg install ruby
+pkg install curl
+pkg install x11-repo
+pkg install root-repo
+pkg install termux-api
+pkg install unstable-repo
+pkg install game-repo
